2382857906aee8ecb92877a7d448ac38e07a75f9
daffaputra09 <daffaputra863@gmail.com>
2023-04-05 15:02:23 +0700

    change trash form size

--- DataGuru/UpdateGuru.cs
+++ DataGuru/UpdateGuru.cs
@@ -69,6 +69,7 @@ namespace DataGuru
                 cmd.Parameters.AddWithValue("@updated_at", DateTime.Now);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Data Berhasil Diubah");
+                this.Close();
             }
             catch (Exception ex)
             {
