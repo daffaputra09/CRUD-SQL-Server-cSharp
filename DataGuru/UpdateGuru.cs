2cc0bb2876a93e1fa695eb1af01b3caf2b0e23f4
daffaputra09 <daffaputra863@gmail.com>
2023-04-12 19:52:11 +0700

    add check nip on update guru

--- DataGuru/UpdateGuru.cs
+++ DataGuru/UpdateGuru.cs
@@ -47,33 +47,52 @@ namespace DataGuru
 
         private void ActionSimpan_Click(object sender, EventArgs e)
         {
-            SqlConnection conn = a.GetConn();
-            try
+            if (FormNip.Text == "" || FormNama.Text == "" || FormGaji.Text == "" || FormTL.Text == "" || FormMapel.Text == "")
             {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE tb_guru SET nip=@nip, nama=@nama, jenis_kelamin=@jenis_kelamin, tanggal_lahir=@tanggal_lahir, mata_pelajaran=@mata_pelajaran, Gaji=@gaji, updated_at=@updated_at WHERE id=@id ", conn);
-                cmd.Parameters.AddWithValue("@id", Formid.Text);
-                cmd.Parameters.AddWithValue("@nip", FormNip.Text);
-                cmd.Parameters.AddWithValue("@nama", FormNama.Text);
-                if(laki.Checked == true)
-                {
-                    cmd.Parameters.AddWithValue("@jenis_kelamin", laki.Text);
-                }
-                if (perempuan.Checked == true)
-                {
-                    cmd.Parameters.AddWithValue("@jenis_kelamin", perempuan.Text);
-                }
-                cmd.Parameters.AddWithValue("@tanggal_lahir", FormTL.Text);
-                cmd.Parameters.AddWithValue("@mata_pelajaran", FormMapel.Text);
-                cmd.Parameters.AddWithValue("@gaji", FormGaji.Text);
-                cmd.Parameters.AddWithValue("@updated_at", DateTime.Now);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Data Berhasil Diubah");
-                this.Close();
+                MessageBox.Show("Data Belum Lengkap");
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message);
+                string nip = FormNip.Text;
+                string CekNip = $"SELECT COUNT(*) FROM tb_guru WHERE nip = '{nip}'";
+                SqlConnection conn = a.GetConn();
+                try
+                {
+                    conn.Open();
+                    SqlCommand cek = new SqlCommand(CekNip, conn);
+                    int count = (int)cek.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        SqlCommand cmd = new SqlCommand("UPDATE tb_guru SET nip=@nip, nama=@nama, jenis_kelamin=@jenis_kelamin, tanggal_lahir=@tanggal_lahir, mata_pelajaran=@mata_pelajaran, Gaji=@gaji, updated_at=@updated_at WHERE id=@id ", conn);
+                        cmd.Parameters.AddWithValue("@id", Formid.Text);
+                        cmd.Parameters.AddWithValue("@nip", FormNip.Text);
+                        cmd.Parameters.AddWithValue("@nama", FormNama.Text);
+                        if(laki.Checked == true)
+                        {
+                            cmd.Parameters.AddWithValue("@jenis_kelamin", laki.Text);
+                        }
+                        if (perempuan.Checked == true)
+                        {
+                            cmd.Parameters.AddWithValue("@jenis_kelamin", perempuan.Text);
+                        }
+                        cmd.Parameters.AddWithValue("@tanggal_lahir", FormTL.Text);
+                        cmd.Parameters.AddWithValue("@mata_pelajaran", FormMapel.Text);
+                        cmd.Parameters.AddWithValue("@gaji", FormGaji.Text);
+                        cmd.Parameters.AddWithValue("@updated_at", DateTime.Now);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil Diubah");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("NIP yang dimasukkan sudah ada", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
     }
